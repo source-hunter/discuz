b0b380ee769fae6d8cf1e90332ea7d90079a1544
source-hunter <root@leavesongs.com>
2015-03-09 17:33:07 +0800

    2015-12-25

---
+++ data/sysdata/cache_domain.php
@@ -0,0 +1,28 @@
+<?php
+//Discuz! cache file, DO NOT modify me!
+//Identify: fc47337d63dd2ccb6cbf999f65d8af29
+
+$domain = array (
+  'defaultindex' => 'forum.php',
+  'holddomain' => 'www|*blog*|*space*|*bbs*',
+  'list' => 
+  array (
+  ),
+  'app' => 
+  array (
+    'portal' => '',
+    'forum' => '',
+    'group' => '',
+    'home' => '',
+    'default' => '',
+  ),
+  'root' => 
+  array (
+    'home' => '',
+    'group' => '',
+    'forum' => '',
+    'topic' => '',
+    'channel' => '',
+  ),
+);
+?>
---
+++ uc_server/data/config.inc.php
@@ -0,0 +1,18 @@
+<?php 
+define('UC_DBHOST', 'localhost');
+define('UC_DBUSER', 'root');
+define('UC_DBPW', 'root');
+define('UC_DBNAME', 'ultrax');
+define('UC_DBCHARSET', 'utf8');
+define('UC_DBTABLEPRE', 'pre_ucenter_');
+define('UC_COOKIEPATH', '/');
+define('UC_COOKIEDOMAIN', '');
+define('UC_DBCONNECT', 0);
+define('UC_CHARSET', 'utf-8');
+define('UC_FOUNDERPW', 'd7f31ac97e37ef36233887b5fc3d422f');
+define('UC_FOUNDERSALT', 'd0Edgd');
+define('UC_KEY', 'K0OdsdVc54W89bt3I0G8m6r2fbG2OfVbt5wfjdObTbj5x5leS7UbMeZaA2o7w3W6');
+define('UC_SITEID', '101dZducV4J8Hbg350R8Z6Z2tbM2IfYbh58fCdmbWbO595feh7fbReha82P7w3K6');
+define('UC_MYKEY', 'd0NdDdZc6408obr3Y0M8q6P2UbE2XfqbH5ffddIbEbi5e5dei7hb1eqas2I7n3k6');
+define('UC_DEBUG', false);
+define('UC_PPP', 20);
